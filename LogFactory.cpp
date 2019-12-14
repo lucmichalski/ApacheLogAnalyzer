9a3ca432744e640c325fa5ab933a21577df38bd2
Loïc DUBOIS-TERMOZ <loic.duboistermoz@outlook.fr>
2019-12-14 14:38:32 +0100

    Add LogFactory class

---
+++ LogFactory.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+                  LogFactory - Fabrique de la classe Log
+							 -------------------
+	début                : 11/12/2019
+	copyright            : (C) 2019 par :
+                            - Milène DAUGAN
+                            - Loïc DUBOIS-TERMOZ
+	e-mail               :
+                            - milene.daugan@insa-lyon.fr
+                            - loic.dubois-termoz@insa-lyon.fr
+*************************************************************************/
+
+#include "LogFactory.h"
--- LogFactory.h
+++ LogFactory.h
@@ -1,70 +1,38 @@
 /*************************************************************************
-						   LogFactory.h  -  Classe virtuelle
+                  LogFactory - Fabrique de la classe Log
 							 -------------------
 	début                : 11/12/2019
-	copyright            : (C) 2019 par DAUGAN
-	e-mail               : $EMAIL$
+	copyright            : (C) 2019 par :
+                            - Milène DAUGAN
+                            - Loïc DUBOIS-TERMOZ
+	e-mail               :
+                            - milene.daugan@insa-lyon.fr
+                            - loic.dubois-termoz@insa-lyon.fr
 *************************************************************************/
-
-//---------- Interface de la classe <LogFactory> (fichier LogFactory.h) --------
-#if ! defined ( LOGFACTORY_H )
-#define LOGFACTORY_H
+//------ Interface de la classe <LogFactory> (fichier LogFactory.h) ------
+#ifndef APACHELOGANALYZER_LOGFACTORY_H
+#define APACHELOGANALYZER_LOGFACTORY_H
 //--------------------------------------------------- Interfaces utilisées
-
-//------------------------------------------------------------- Constantes
-
-//------------------------------------------------------------------ Types
 
 //------------------------------------------------------------------------
 // Rôle de la classe <LogFactory>
-//Cette classe virtuelle permet de gérer les classes LogFactoryImpl, Log,
-//Request, Document
+//  LogFactory a pour rôle d'instancier la classe Log ainsi que les classes
+//  associées (Document et Request) en se basant sur les lignes des logs
+//  importées.
 //------------------------------------------------------------------------
-class LogFactory :
-{
+
+class LogFactory {
 //----------------------------------------------------------------- PUBLIC
 public:
 //----------------------------------------------------- Méthodes publiques
-	// type Méthode ( liste des paramètres );
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-	virtual void Methode () const = 0;
-
-//------------------------------------------------- Surcharge d'opérateurs
-	LogFactory & operator = ( const LogFactory & unLogFactory );
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
 
 //-------------------------------------------- Constructeurs - destructeur
-	LogFactory ( const LogFactory & unLogFactory );
-	// Mode d'emploi (constructeur de copie) :
-	//
-	// Contrat :
-	//
-
-	LogFactory ( );
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	virtual ~LogFactory ( );
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
 
 //------------------------------------------------------------------ PRIVE
+
 protected:
-//----------------------------------------------------- Méthodes protégées
-
 //----------------------------------------------------- Attributs protégés
-
 };
 
-//-------------------------------- Autres définitions dépendantes de <LogFactory>
-#endif // LOGFACTORY_H
+
+#endif //APACHELOGANALYZER_LOGFACTORY_H
