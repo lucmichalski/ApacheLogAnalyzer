4545d328677b9a21d6a2548746943add7e43f66d
Loïc DUBOIS-TERMOZ <loic.duboistermoz@outlook.fr>
2019-12-14 14:37:30 +0100

    Add DocumentHits and ApplicationArguments classes

---
+++ ApplicationArguments.h
@@ -0,0 +1,96 @@
+/*************************************************************************
+ApplicationArguments - Arguments spécifiés à l'application lors de son lancement
+							 -------------------
+	début                : 11/12/2019
+	copyright            : (C) 2019 par :
+                            - Milène DAUGAN
+                            - Loïc DUBOIS-TERMOZ
+	e-mail               :
+                            - milene.daugan@insa-lyon.fr
+                            - loic.dubois-termoz@insa-lyon.fr
+*************************************************************************/
+// Interface de la classe <ApplicationArguments> (fichier ApplicationArguments.h)
+#ifndef APACHELOGANALYZER_APPLICATIONARGUMENTS_H
+#define APACHELOGANALYZER_APPLICATIONARGUMENTS_H
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+
+using namespace std;
+
+//------------------------------------------------------------------------
+// Rôle de la classe <ApplicationArguments>
+//  ApplicationArguments contient l'ensemble des données sur les arguments
+//  de l'application spécifiés au lancement de celle-ci. Il s'agit
+//  d'un simple conteneur sans logique.
+//------------------------------------------------------------------------
+
+class ApplicationArguments {
+//----------------------------------------------------------------- PUBLIC
+public:
+//----------------------------------------------------- Méthodes publiques
+    /**
+     * @return La référence sur le nom du fichier de log à importer
+     */
+    const string &getLogFileName() const {
+        return logFileName;
+    }
+
+    /**
+     * @return "true" si les extensions css, js et image doivent être exclus, "false" sinon
+     */
+    bool isExcludeExtensions() const {
+        return excludeExtensions;
+    }
+
+    /**
+     * @return Un nombre entre 0 et 23 correspondant au créneau horaire sur lequel on souhaite conserver les
+     *      logs, ou -1 si aucun filtrage ne doit être effectué
+     */
+    int getFilterHour() const {
+        return filterHour;
+    }
+
+    /**
+     * @return Le nom du fichier .dot à générer. Si la chaîne est vide, alors aucun fichier ne sera généré
+     */
+    const string &getDotFileName() const {
+        return dotFileName;
+    }
+
+//-------------------------------------------- Constructeurs - destructeur
+    /**
+     * Constructeur des arguments de l'application.
+     *
+     * @param theLogFileName Le nom du fichier de log à importer
+     * @param isExcludeExtensions "true" si les extensions css, image et js doivent être exclus, "false" sinon
+     * @param theFilterHour L'heure à utiliser pour le filtrage du log (entre 0 et 23), -1 si aucun filtrage
+     * @param theDotFileName Le nom du fichier .dot (de l'application GraphViz) à générer, chaîne vide si aucun fichier
+     *      à générer
+     */
+    ApplicationArguments(string &theLogFileName, bool isExcludeExtensions, int theFilterHour, string &theDotFileName)
+            : logFileName(theLogFileName), excludeExtensions(isExcludeExtensions), filterHour(theFilterHour),
+              dotFileName(theDotFileName) {}
+
+    /**
+     * Constructeur par copie.
+     * Comportement par défaut.
+     */
+    ApplicationArguments(const ApplicationArguments &applicationArguments) = default;
+
+    /**
+     * Destructeur de l'instance.
+     * Comportement par défaut.
+     */
+    virtual ~ApplicationArguments() = default;
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Attributs protégés
+    string logFileName;
+    bool excludeExtensions;
+    int filterHour = -1;
+    string dotFileName;
+};
+
+#endif //APACHELOGANALYZER_APPLICATIONARGUMENTS_H
---
+++ DocumentHits.h
@@ -0,0 +1,74 @@
+/*************************************************************************
+    DocumentHits - Association entre un document et son nombre de hits
+							 -------------------
+	début                : 11/12/2019
+	copyright            : (C) 2019 par :
+                            - Milène DAUGAN
+                            - Loïc DUBOIS-TERMOZ
+	e-mail               :
+                            - milene.daugan@insa-lyon.fr
+                            - loic.dubois-termoz@insa-lyon.fr
+*************************************************************************/
+//---- Interface de la classe <DocumentHits> (fichier DocumentHits.h) ----
+#ifndef APACHELOGANALYZER_DOCUMENTHITS_H
+#define APACHELOGANALYZER_DOCUMENTHITS_H
+//--------------------------------------------------- Interfaces utilisées
+#include "Document.h"
+
+//------------------------------------------------------------------------
+// Rôle de la classe <DocumentHits>
+//  DocumentHits est un conteneur associant un document à son nombre de
+//  hits.
+//------------------------------------------------------------------------
+
+class DocumentHits {
+//----------------------------------------------------------------- PUBLIC
+public:
+//----------------------------------------------------- Méthodes publiques
+    /**
+     * @return La référence constante au document associé au nombre de hits
+     */
+    const Document &getDocument() const {
+        return document;
+    }
+
+    /**
+     * @return Le nombre de hits associé au document
+     */
+    int getNbHits() const {
+        return nbHits;
+    }
+
+//-------------------------------------------- Constructeurs - destructeur
+    /**
+     * Constructeur de l'association document - nombre de hits
+     *
+     * @param theDocument La référence au document à associer au nombre de hits
+     * @param theNbHits Le nombre de hits du document associé
+     */
+    DocumentHits(Document &theDocument, int theNbHits)
+            : document(theDocument), nbHits(theNbHits) {}
+
+    /**
+     * Constructeur par copie.
+     * Comportement par défaut.
+     *
+     * @param documentHits La référence au document hits à copier
+     */
+    DocumentHits(const DocumentHits &documentHits) = default;
+
+    /**
+     * Destructeur de l'instance de la classe.
+     * Comportement par défaut.
+     */
+    virtual ~DocumentHits() = default;
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Attributs protégés
+    Document &document;
+    int nbHits = 0;
+};
+
+#endif //APACHELOGANALYZER_DOCUMENTHITS_H
