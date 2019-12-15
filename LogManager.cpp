4a0bdcbca133961412b73f76aed813dc7ee8c8ad
Loïc DUBOIS-TERMOZ <loic.duboistermoz@outlook.fr>
2019-12-15 15:42:25 +0100

    Update and set final structure of LogManager class

--- LogManager.cpp
+++ LogManager.cpp
@@ -11,13 +11,58 @@
 *************************************************************************/
 #include "LogManager.h"
 
-const Log *LogManager::FilterByExtensions(const Log &_log) {
-    // TODO : Lister les extensions à filtrer
+/**
+ * Créer une copie de l'instance de la classe Log spécifiée puis compare les extensions des documents source
+ * et destination et de chaque requête avec les extensions passées en paramètre. La requête est retirée de
+ * la copie de l'instance de la classe Log si l'une des extensions des documents correspond.
+ */
+const Log *LogManager::FilterByExtensions(const Log &_log, const vector<const string> &extensions) const {
     // TODO : Clonage du Log + Filtrage sur les extensions listés
     return nullptr;
 }
 
-const Log *LogManager::FilterByHour(const Log &_log, int hour) {
+/**
+ * Créer une copie de l'instance de la classe Log spécifiée puis vérifie la validité du paramètre hour.
+ * Si le paramètre n'est pas compris dans l'interval [0,23], alors une exception est levée.
+ * Sinon compare les heures des requêtes avec l'heure spécifiée. Si les heures ne sont pas égales, alors
+ * la requête est retirée de la copie de l'instance de la classe Log.
+ */
+const Log *LogManager::FilterByHour(const Log &_log, int hour) const {
     // TODO : Clonage du Log + Filtrage sur l'heure spécifiée
     return nullptr;
+}
+
+/**
+ * Initialise un vecteur de la taille du nombre de documents stockés dans l'instance de la classe Log puis
+ * le remplit d'associations entre un document et son nombre de hits via les requêtes enregistrées.
+ */
+const vector<const DocumentHits *> *LogManager::GetDocumentsHits(const Log &log) const {
+    // TODO : Construire un vecteur de taille le nombre de documents du Log puis le remplir
+    return nullptr;
+}
+
+/**
+ * Détruit l'ensemble des instances de la classe DocumentHits avant de détuire le vecteur.
+ */
+void LogManager::DeleteDocumentsHits(const vector<const DocumentHits *> *documentsHits) const {
+    // TODO : Destruction du vecteur ainsi que les DocumentHits contenus
+}
+
+/**
+ * Pour chaque document, compte le nombre d'occurence du document en comparant les documents source
+ * et destination de chaque requête avec ce premier. Le programme créer ensuite une instance de la classe
+ * DocumentHits à partir du nombre de hits et du document et le stock dans le vecteur correspondant.
+ * Une fois le traitement terminé, le vecteur est retourné.
+ */
+const vector<const DocumentHits *> *LogManager::SortByHits(const vector<const DocumentHits *> &documentsHits) const {
+    // TODO : Tri du vecteur par ordre décroissant du nombre de hits (se baser sur la STL si possible)
+    return nullptr;
+}
+
+/**
+ * // TODO : Spécifications à détailler !
+ */
+int LogManager::CountTimelessRequestsEquals(const Log &log, const Request &request) const {
+    // TODO : Déterminer la pondération de la requête à partir de l'instance de la classe Log spécifiée
+    return 0;
 }
--- LogManager.h
+++ LogManager.h
@@ -13,6 +13,7 @@
 #define APACHELOGANALYZER_LOGMANAGER_H
 
 #include "Log.h"
+#include "DocumentHits.h"
 
 /**
  * LogManager met à disposition un ensemble de services permettant de gérer tout l'aspect traitement
@@ -20,10 +21,62 @@
  */
 class LogManager {
 public:
-    const Log *FilterByExtensions(const Log &_log);
+    /**
+     * Créer une copie de l'instance de la classe Log spécifiée puis applique un filtrage sur
+     * les requêtes en excluant les extensions des documents avant de retourner la copie.
+     *
+     * @param _log L'instance de la classe Log correspondant au fichier de log importé
+     * @return La copie de l'instance de la classe Log spécifiée filtrée sur les extensions
+     */
+    const Log *FilterByExtensions(const Log &_log, const vector<const string> &extensions) const;
 
-    const Log *FilterByHour(const Log &_log, int hour);
-protected:
+    /**
+     * Créer une copie de l'instance de la classe Log spécifiée puis applique un filtrage dessus
+     * en excluant les requêtes dont l'heure enregistrée n'est pas incluse dans le créneau horaire
+     * [hour, hour + 1[.
+     *
+     * @param _log L'instance de la classe Log correspondant au fichier de log importé
+     * @param hour Le créneaux horaire sur lequel le filtrage doit se baser
+     * @return La copie de l'instance de la classe Log spécifiée filtrée sur le créneau horaire
+     * @throws exception Si le paramètre hour n'est pas inclus dans l'ensemble [0,23]
+     */
+    const Log *FilterByHour(const Log &_log, int hour) const;
+
+    /**
+     * Construit un vecteur d'association entre un document et son nombre de hits et le retourne.
+     * Les pointeurs du vecteurs peuvent être modifiés pour pouvoir le trier par exemple mais aucun
+     * d'entre eux ne devrait être retiré de la collection.
+     *
+     * @param log L'instance de la classe Log représentant le fichier de log importé
+     * @return Un vecteur d'association entre un document et son nombre de hits
+     */
+    const vector<const DocumentHits *> *GetDocumentsHits(const Log &log) const;
+
+    /**
+     * Détruit le vecteur ainsi que les associations entre un document et son nombre de hits DocumentHits
+     * contenus dans le vecteur.
+     *
+     * @param documentsHits Le vecteur d'associations entre un document et son nombre de hits
+     */
+    void DeleteDocumentsHits(const vector<const DocumentHits *> *documentsHits) const;
+
+    /**
+     * Tri le vecteur d'associations entre un document et son nombre de hits par ordre décroissant du nombre
+     * de hits.
+     *
+     * @param documentsHits La vecteur d'associations entre un document et son nombre de hits
+     * @return Le pointeur sur le vecteur passé en paramètre trié
+     */
+    const vector<const DocumentHits *> *SortByHits(const vector<const DocumentHits *> &documentsHits) const;
+
+    /**
+     * // TODO : Spécifications à détailler !
+     *
+     * @param log
+     * @param request
+     * @return
+     */
+    int CountTimelessRequestsEquals(const Log &log, const Request &request) const;
 };
 
 
