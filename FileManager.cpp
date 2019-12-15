45c076355cfb86f4d0a416b83e89474eb1522a81
Loïc DUBOIS-TERMOZ <loic.duboistermoz@outlook.fr>
2019-12-15 14:35:47 +0100

    Update and set final structure of FileManager

--- FileManager.cpp
+++ FileManager.cpp
@@ -1,4 +1,4 @@
-/*************************************************************************
+/**********void CreateDotFile(const string &dotFileName, const Log &log) const***************************************************************
    FileManager - Service sur le traitement des fichiers de l'application
 							 -------------------
 	début                : 11/12/2019
@@ -10,3 +10,46 @@
                             - loic.dubois-termoz@insa-lyon.fr
 *************************************************************************/
 #include "FileManager.h"
+
+/**
+ * Ouvre le fichier en lecture et stock chaque ligne lue dans un ensemble non-ordonné puis le retourne
+ * en fermant au préalable le fichier ouvert.
+ */
+Log *FileManager::ImportLogFile(const string &fileName) const {
+    // TODO : Import du fichier de log sous la forme d'une instance de la classe Log
+    return nullptr;
+}
+
+/**
+ * Ouvre le fichier en écriture en écrasant tout son contenu puis :
+ * - Ecrit la première ligne de début du digraph ;
+ * - Ecrit la liste des documents utilisés représentés par des noeuds ;
+ * - Ecrit la liste des requêtes pondérés par une quantité représentés par des associations entre deux noeuds ;
+ * - Ecrit la dernière ligne de fin du digraph.
+ * Et enfin ferme le fichier.
+ */
+void FileManager::CreateDotFile(const string &dotFileName, const Log &log) const {
+    // TODO : Création du fichier .dot en fonction de l'instance de la classe Log spécifiée
+    // SPECIFICATIONS A PRECISER !!! concernant la déléguation du traitement du Log pour le fichier .dot
+}
+
+/**
+ * Ligne le fichier et s'arrête à chaque caractère de fin de ligne pour stocker l'ensemble des caractères
+ * lus dans une chaîne de caractère elle même stockée dans un ensemble non-ordonné.
+ * Une fois la lecture terminée, l'ensemble est retourné.
+ */
+const unordered_set<const string * const> *FileManager::extractLogLines(const ifstream &logFile) const {
+    // TODO : Extraction des lignes de log du fichier à importer
+    return nullptr;
+}
+
+/**
+ * Détruit les ligne de log représentés par des chaînes de caractères puis supprime l'ensemble
+ * non-ordonné.
+ */
+void FileManager::deleteLogLines(const unordered_set<const string * const> *logLines) const {
+    for (const string * const logLine : *logLines) {
+        delete logLine;
+    }
+    delete logLines;
+}
--- FileManager.h
+++ FileManager.h
@@ -11,26 +11,64 @@
 *************************************************************************/
 #ifndef APACHELOGANALYZER_FILEMANAGER_H
 #define APACHELOGANALYZER_FILEMANAGER_H
-//--------------------------------------------------- Interfaces utilisées
 
-//------------------------------------------------------------------------
-// Rôle de la classe <FileManager>
-//  FileManager met à disposition un ensemble de services permettant
-//  de gérer tout l'aspect traitement de fichier de l'application
-//  (import du fichier de log, création du fichier .dot, etc).
-//------------------------------------------------------------------------
+#include "Log.h"
 
+/**
+ * FileManager met à disposition un ensemble de services permettant de gérer tout l'aspect
+ * traitement de fichier de l'application (import du fichier de log, création du fichier .dot, etc).
+ */
 class FileManager {
-//----------------------------------------------------------------- PUBLIC
 public:
-//----------------------------------------------------- Méthodes publiques
+    /**
+     * Récupère et importe l'ensemble des informations contenues dans le fichier de log spécifié.
+     *
+     * @param fileName Le nom du fichier de log à importer
+     * @return Les informations du fichier de log importé sous la forme d'une instance de la classe Log
+     * @throws exception S'il n'est pas possible de lire le fichier ou qu'il comporte des erreurs
+     *  (fichier introuvable, problème de permission, le format d'une ligne de log est incorrect, chemin incorrect)
+     *  // TODO : exceptions OK ?
+     */
+    Log *ImportLogFile(const string &fileName) const;
 
-//-------------------------------------------- Constructeurs - destructeur
+    /**
+     * Créer le fichier .dot correspondant à l'instance de la classe Log spécifiée. Écrase le fichier existant.
+     *
+     * @param dotFileName Le nom du fichier .dot à créer
+     * @param log L'instance de la classe Log à utiliser pour la génération du fichier .dot
+     * // TODO : Param log à changer ? (déléguer la responsabilité de traiter le Log)
+     * @throws exception Si un problème de permissions ou de chemin incorrect est détecté
+     * // TODO : exceptions OK ?
+     */
+    void CreateDotFile(const string &dotFileName, const Log &log) const;
 
-//------------------------------------------------------------------ PRIVE
+    /**
+     * Constructeur par défaut de la classe.
+     * Comportement par défaut.
+     */
+    FileManager() = default;
 
+    /**
+     * Destructeur de la classe.
+     * Comportement par défaut.
+     */
+    ~FileManager() = default;
 protected:
-//----------------------------------------------------- Attributs protégés
+    /**
+     * Extrait les lignes du fichier de log à importer individuellement et les stock dans un ensemble non-ordonné.
+     *
+     * @param logFile Le fichier de log contenant les lignes à importer
+     * @return Un ensemble non-ordonné de lignes du fichier de log importé
+     * // TODO : Exception(s) levée(s) ?
+     */
+    const unordered_set<const string * const> *extractLogLines(const ifstream &logFile) const;
+
+    /**
+     * Détruit l'ensemble ainsi que les chaînes qu'il contient.
+     *
+     * @param logLines L'ensemble de lignes de log à détruire
+     */
+    void deleteLogLines(const unordered_set<const string * const> *logLines) const;
 };
 
 
